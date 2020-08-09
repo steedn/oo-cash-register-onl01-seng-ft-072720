fe45cbfc45b4985f33e5cd74c68618da00a6c346
Nathan Steed <steedn@outlook.com>
2020-08-09 20:52:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class  CashRegister
     new_disc = @discount.to_f / 100
     new_total = @total - @total*new_disc
     @total = new_total
-    "After the discount, the total comes to $#{@total}."
+    "After the discount, the total comes to $#{@total.to_i}."
     # binding.pry
   end
 end
