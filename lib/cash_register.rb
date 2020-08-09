c45d0890584314c2d26329c24a98b2c7467a03e6
Nathan Steed <steedn@outlook.com>
2020-08-09 20:38:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,14 @@
 class  CashRegister
 
-  attr_accessor :total
+  attr_accessor :total, :discount
 
   def initialize(f = nil)
+    @discount = f
     @total = 0
   end
 
   def discount(f = nil)
+
   end
 
 end
